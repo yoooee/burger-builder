80b6fc30ea628b6292e6357cab9014abe3a374dc
Josef Staroba <jstaroba@centene.com>
2019-07-16 11:51:30 -0500

    156. Make data conversion easier to read/understand

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -25,12 +25,31 @@ const burger = ( props ) => {
     BurgerIngredient with the current key.
    */
 
-  const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-      return [...Array(props.ingredients[igKey])].map((_, i) => {
-        return <BurgerIngredient key={igKey + i} type={igKey} />;
-      })
-    })
+  const uniqueIngredients = Object.keys(props.ingredients); // ["salad", "bacon", "cheese", "meat"]
+
+  const transformedIngredients = uniqueIngredients.map(ingredientKey => {
+    const ingredientQuantity = props.ingredients[ingredientKey]; // Get the value of each key.
+
+    const ingredientQuantityAllocation = Array(ingredientQuantity); // Allocates space for an array of length "ingredientQuantity"
+
+    // Destructure the ingredientQuantityAllocation to create an actual array of undefineds.
+    const ingredientQuantityArray = [...ingredientQuantityAllocation];  // [undefined] or [undefined, undefined] depending on the value of ingredientQuantityAllocation.
+
+    const newElement = ingredientQuantityArray.map((_, i) => {
+      return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
+    });
+
+    return newElement;
+  });
+
+  // A more concise way to do the same thing as above.
+  //const transformedIngredients = Object.keys(props.ingredients)
+    //.map(igKey => {
+      //return [...Array(props.ingredients[igKey])].map((_, i) => {
+        //return <BurgerIngredient key={igKey + i} type={igKey} />;
+      //})
+    //});
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
