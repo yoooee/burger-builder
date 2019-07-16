0ac46b6c768cfdc1b71f047252e872dcc59e1aff
Josef Staroba <jstaroba@centene.com>
2019-07-16 09:38:20 -0500

    155. Starting the Burger Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from './BurgerIngredient';
+import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
   render() {
+  console.log('??????????????????????', classes);
 
   let ingredient = null;
 
