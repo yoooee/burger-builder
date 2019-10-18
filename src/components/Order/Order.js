83c97fc573b2213896ad6b10909c7fd9506b428e
Joe Staroba <jstaroba@centene.com>
2019-10-18 09:01:48 -0500

    257.  Implementing Navigation Links

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = props => (
-  <div className="classes.Order">
+  <div className={classes.Order}>
     <p>Ingredients: Salad (1)</p>
     <p>
       Price: <strong>USD 5.45</strong>
